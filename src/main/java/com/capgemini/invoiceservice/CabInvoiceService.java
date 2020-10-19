699535e99365dbb84fed17ff17a5f1cba826c417
Patel210 <pranavpatel2101997@gmail.com>
2020-10-19 18:30:30 +0530

    Modified the code to now have separate rates for premium rides

--- src/main/java/com/capgemini/invoiceservice/CabInvoiceService.java
+++ src/main/java/com/capgemini/invoiceservice/CabInvoiceService.java
@@ -7,22 +7,33 @@ import com.capgemini.exception.InvoiceServiceException.ExceptionType;
 import com.capgemini.exception.RepositoryException;
 import com.capgemini.myrides.RepositoryService;
 import com.capgemini.myrides.Ride;
+import com.capgemini.myrides.Ride.ServiceType;
 
 public class CabInvoiceService {
 
 	private final double COST_PER_KM = 10.0;
 	private final int COST_PER_MIN = 1;
 	private final double MINIMUM_FARE = 5.0;
+	private final double COST_PER_KM_PREMIUM = 15.0;
+	private final int COST_PER_MIN_PREMIUM = 2;
+	private final double MINIMUM_FARE_PREMIUM = 20.0;
+	
 	private RepositoryService repoService = new RepositoryService();
 
-	public double calculateFare(double distance, int time) {
-		return Math.max((COST_PER_KM * distance + COST_PER_MIN * time), MINIMUM_FARE);
+	public double calculateFare(double distance, int time, ServiceType type) {
+		if(type.equals(ServiceType.REGULAR)) {
+			return Math.max((COST_PER_KM * distance + COST_PER_MIN * time), MINIMUM_FARE);
+		}
+		else if(type.equals(ServiceType.PREMIUM)) {
+			return Math.max((COST_PER_KM_PREMIUM * distance + COST_PER_MIN_PREMIUM * time), MINIMUM_FARE_PREMIUM);
+		}
+		return 0;
 	}
 
 	public double calculateTotalFare(List<Ride> rides) {
 		double totalFare = 0.0;
 		for (Ride ride : rides) {
-			totalFare += calculateFare(ride.getDistance(), ride.getTime());
+			totalFare += calculateFare(ride.getDistance(), ride.getTime(), ride.type);
 		}
 		return totalFare;
 	}
@@ -43,7 +54,7 @@ public class CabInvoiceService {
 		double totalFare = calculateTotalFare(myRides);
 		return new InvoiceSummary(myRides.size(), totalFare);
 	}
-
+	
 	public RepositoryService getRepoService() {
 		return repoService;
 	}
--- src/main/java/com/capgemini/myrides/Ride.java
+++ src/main/java/com/capgemini/myrides/Ride.java
@@ -1,13 +1,19 @@
 package com.capgemini.myrides;
 
 public class Ride {
-
+	
+	public ServiceType type;
 	private double distance;
 	private int time;
+	
+	public enum ServiceType{
+		REGULAR, PREMIUM;
+	}
 
-	public Ride(double distance, int time) {
+	public Ride(double distance, int time, ServiceType type) {
 		this.distance = distance;
 		this.time = time;
+		this.type = type;
 	}
 
 	public double getDistance() {
