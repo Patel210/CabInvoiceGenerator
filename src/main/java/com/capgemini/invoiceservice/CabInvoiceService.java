70263ae4755e9f3459074f69157f912300592664
Patel210 <pranavpatel2101997@gmail.com>
2020-10-19 15:38:50 +0530

    Modified the functionality to return minimum fare in case the fare based on distance and time is less than the minimum fare

--- src/main/java/com/capgemini/invoiceservice/CabInvoiceService.java
+++ src/main/java/com/capgemini/invoiceservice/CabInvoiceService.java
@@ -4,8 +4,9 @@ public class CabInvoiceService {
 
 	private final double COST_PER_KM = 10.0;
 	private final int COST_PER_MIN = 1;
+	private final double MINIMUM_FARE = 5.0;
 
 	public double calculateFare(double distance, int time) {
-		return COST_PER_KM * distance + COST_PER_MIN * time;
+		return Math.max((COST_PER_KM * distance + COST_PER_MIN * time), MINIMUM_FARE);
 	}
 }
