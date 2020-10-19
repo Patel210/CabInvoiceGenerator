45e6709567e21b791d5637f4d6d10947ba719c6c
Patel210 <pranavpatel2101997@gmail.com>
2020-10-19 18:31:30 +0530

    Added test case for premium rides of the user

--- src/test/java/com/capgemini/invoiceservice/InvoiceServiceTest.java
+++ src/test/java/com/capgemini/invoiceservice/InvoiceServiceTest.java
@@ -9,10 +9,9 @@ import org.junit.Before;
 import org.junit.Test;
 
 import com.capgemini.exception.InvoiceServiceException;
-import com.capgemini.exception.InvoiceServiceException.ExceptionType;
 import com.capgemini.exception.RepositoryException;
-import com.capgemini.exception.RepositoryException.*;
 import com.capgemini.myrides.Ride;
+import com.capgemini.myrides.Ride.ServiceType;
 
 public class InvoiceServiceTest {
 
@@ -27,7 +26,7 @@ public class InvoiceServiceTest {
 	public void givenDistanceAndTime_ShouldReturnTheFare() {
 		double distance = 5.0;
 		int time = 10;
-		double fare = cabInvoiceService.calculateFare(distance, time);
+		double fare = cabInvoiceService.calculateFare(distance, time, ServiceType.REGULAR);
 		assertEquals(60.0, fare, 0.0);
 	}
 
@@ -35,13 +34,15 @@ public class InvoiceServiceTest {
 	public void givenDistanceAndTime_BelowMinimumFareThresholdCondition_ShouldReturnTheMinimumFare() {
 		double distance = 0.3;
 		int time = 1;
-		double fare = cabInvoiceService.calculateFare(distance, time);
+		double fare = cabInvoiceService.calculateFare(distance, time, ServiceType.REGULAR);
 		assertEquals(5.0, fare, 0.0);
 	}
 
 	@Test
 	public void givenMultipleRides_ShouldCalculateTotalFare() {
-		Ride[] rides = { new Ride(5.0, 10), new Ride(0.3, 1), new Ride(10.0, 20) };
+		Ride[] rides = { new Ride(5.0, 10, ServiceType.REGULAR),
+						 new Ride(0.3, 1, ServiceType.REGULAR),
+						 new Ride(10.0, 20, ServiceType.REGULAR) };
 		List<Ride> myRides = Arrays.asList(rides);
 		double fare = cabInvoiceService.calculateTotalFare(myRides);
 		assertEquals(185.0, fare, 0.0);
@@ -49,7 +50,9 @@ public class InvoiceServiceTest {
 
 	@Test
 	public void givenRides_ShouldReturnInvoiceSummary() {
-		Ride[] rides = { new Ride(5.0, 10), new Ride(0.3, 1), new Ride(10.0, 20) };
+		Ride[] rides = { new Ride(5.0, 10, ServiceType.REGULAR),
+				         new Ride(0.3, 1, ServiceType.REGULAR),
+				         new Ride(10.0, 20, ServiceType.REGULAR) };
 		List<Ride> myRides = Arrays.asList(rides);
 		InvoiceSummary summary;
 		try {
@@ -62,9 +65,9 @@ public class InvoiceServiceTest {
 	@Test
 	public void givenUserId_ShouldRetunTheInvoiceSummary() {
 		List<Ride> myRides = new ArrayList<Ride>();
-		myRides.add(new Ride(5.0, 10));
-		myRides.add(new Ride(0.3, 1));
-		myRides.add(new Ride(10.0, 20));
+		myRides.add(new Ride(5.0, 10, ServiceType.REGULAR));
+		myRides.add(new Ride(0.3, 1, ServiceType.REGULAR));
+		myRides.add(new Ride(10.0, 20, ServiceType.REGULAR));
 		int id = 1;
 		cabInvoiceService.getRepoService().addUserRides(id, myRides);
 		InvoiceSummary summary;
@@ -78,9 +81,9 @@ public class InvoiceServiceTest {
 	@Test
 	public void givenUserId_WhenNoRideAvailableForTheUser_ShouldThrowAnException() {
 		List<Ride> myRides = new ArrayList<Ride>();
-		myRides.add(new Ride(5.0, 10));
-		myRides.add(new Ride(0.3, 1));
-		myRides.add(new Ride(10.0, 20));
+		myRides.add(new Ride(5.0, 10, ServiceType.REGULAR));
+		myRides.add(new Ride(0.3, 1, ServiceType.REGULAR));
+		myRides.add(new Ride(10.0, 20, ServiceType.REGULAR));
 		int id = 1;
 		cabInvoiceService.getRepoService().addUserRides(id, myRides);
 		InvoiceSummary summary;
@@ -89,5 +92,21 @@ public class InvoiceServiceTest {
 		} catch (RepositoryException e) {
 			assertEquals(RepositoryException.ExceptionType.USER_NOT_FOUND, e.type);
 		} catch (InvoiceServiceException e) {}
+	}
+	
+	@Test
+	public void givenUserId_WithPremiumRides_ShouldReturnTheSummary() {
+		List<Ride> myRides = new ArrayList<Ride>();
+		myRides.add(new Ride(5.0, 10, ServiceType.PREMIUM));
+		myRides.add(new Ride(0.3, 1, ServiceType.PREMIUM));
+		myRides.add(new Ride(10.0, 20, ServiceType.PREMIUM));
+		int id = 1;
+		cabInvoiceService.getRepoService().addUserRides(id, myRides);
+		InvoiceSummary summary;
+		try {
+			summary = cabInvoiceService.generateSummary(id);
+			InvoiceSummary expectedSummary = new InvoiceSummary(3, 305.0);
+			assertEquals(expectedSummary, summary);
+		} catch (RepositoryException | InvoiceServiceException e) {}
 	}
 }
