ae293c645d6f01c0d7a5558307c62623f8bbd71f
Patel210 <pranavpatel2101997@gmail.com>
2020-10-19 16:40:14 +0530

    Added functionality to generate invoice. To achieve that created an Invoice summary class that holds no. of rides, total fare, and average fare per ride. Also, created a custom exception class if no rides were taken

---
+++ src/main/java/com/capgemini/exception/InvoiceServiceException.java
@@ -0,0 +1,23 @@
+package com.capgemini.exception;
+
+public class InvoiceServiceException extends Exception {
+
+	public ExceptionType type;
+
+	public enum ExceptionType {
+		NO_RIDE("Atleast 1 ride required for generating summary");
+
+		private String exceptionMessage;
+
+		ExceptionType(String exceptionMessage) {
+			this.exceptionMessage = exceptionMessage;
+		}
+		public String getExceptionMessage() {
+			return exceptionMessage;
+		}
+	}
+
+	public InvoiceServiceException(ExceptionType type) {
+		this.type = type;
+	}
+}
